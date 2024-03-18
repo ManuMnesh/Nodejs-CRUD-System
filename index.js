733a4cd1a03b5914ca58125eaa1d46a1c5384559
Emmanuel Munene Muthengi <iemmanuelmunene@students.uonbi.ac.ke>
2024-03-18 14:24:29 +0300

    commit_3

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ const pool = mysql.createPool({
   database: "hosteltest",
 });
 
-//Get data from table (students) by national
+//Get all records from table (students)
 app.get("", (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) throw err;
     console.log(`Connected as id ${connection.threadId}`);
 
-    connection.query("SELECT * FROM students", (err, rows) => {
+    connection.query("SELECT * FROM students ", (err, rows) => {
       connection.release(); // return the connection pool
 
       if (!err) {
@@ -33,6 +33,125 @@ app.get("", (req, res) => {
         console.log(err);
       }
     });
+  });
+});
+//Get record from table (students) by id
+app.get("/:id", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`Connected as id ${connection.threadId}`);
+
+    connection.query(
+      "SELECT * FROM students WHERE id = ? ",
+      [req.params.id],
+      (err, rows) => {
+        connection.release(); // return the connection pool
+
+        if (!err) {
+          res.send(rows);
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
+});
+//Get record from table (students) by nationa_id
+app.get("/:national_id_number", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`Connected as id ${connection.threadId}`);
+
+    connection.query(
+      "SELECT * FROM students WHERE national_id_number = ? ",
+      [req.params.national_id_number],
+      (err, rows) => {
+        connection.release(); // return the connection pool
+
+        if (!err) {
+          res.send(rows);
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
+});
+
+//Delete record from table (students) by id
+app.delete("/:id", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`Connected as id ${connection.threadId}`);
+
+    connection.query(
+      "DELETE from students WHERE id = ? ",
+      [req.params.id],
+      (err, rows) => {
+        connection.release(); // return the connection pool
+
+        if (!err) {
+          res.send(
+            `Student with the record ID: ${[req.params.id]} has been removed. `
+          );
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
+});
+
+//Add record in the table (students)
+app.post("", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`Connected as id ${connection.threadId}`);
+
+    const params = req.body;
+
+    connection.query("INSERT INTO students SET ? ", [params], (err, rows) => {
+      connection.release(); // return the connection pool
+
+      if (!err) {
+        res.send(`Student with the name: ${[params.name]} has been added. `);
+      } else {
+        console.log(err);
+      }
+    });
+    console.log(req.body);
+  });
+});
+
+//Update record in the table (students)
+app.put("", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`Connected as id ${connection.threadId}`);
+
+    const {
+      id,
+      name,
+      email,
+      national_id_number,
+      university_name,
+      admission_number,
+    } = req.body;
+
+    connection.query(
+      "UPDATE  students SET name = ? WHERE id = ? ",
+      [name, id],
+      (err, rows) => {
+        connection.release(); // return the connection pool
+
+        if (!err) {
+          res.send(`Student with the name: ${name} has been updated. `);
+        } else {
+          console.log(err);
+        }
+      }
+    );
+    console.log(req.body);
   });
 });
 
